47295bad022a3c2b19fb6867454c33453e0693f8
Pandiyan Murugan <pandiyan.murugan@emsibg.com>
2022-06-30 20:40:34 +0530

    changed build url

--- config/index.js
+++ config/index.js
@@ -1,3 +1,3 @@
 const dev = process.env.NODE_ENV !== 'production'
 
-export const server = dev ? `http://localhost:3000` : `https://yourapp.io`
+export const server = dev ? `http://localhost:3000` : `https://jsonplaceholder.typicode.com/posts`
