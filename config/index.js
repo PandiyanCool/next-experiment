126b1e00c3e63a5fd42b495f5acc31ea07a9f71b
Pandiyan Murugan <pandiyan.murugan@emsibg.com>
2022-06-30 20:42:55 +0530

    Changed to real url

--- config/index.js
+++ config/index.js
@@ -1,3 +1,3 @@
 const dev = process.env.NODE_ENV !== 'production'
 
-export const server = dev ? `http://localhost:3000` : `https://jsonplaceholder.typicode.com/posts`
+export const server = dev ? `http://localhost:3000` : `https://kind-sand-0abdc0b10.1.azurestaticapps.net`
